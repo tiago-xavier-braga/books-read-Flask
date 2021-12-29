ef4dea3853b6cce201e4b5a8d630bcd9ca95d087
Tiago Xavier Braga <braga.tiagoxavier@gmail.com>
2021-12-29 15:59:23 -0300

    deploy

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -15,7 +14,12 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Book %r>' % self.id
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
+def index():
+    books = Todo.query.order_by(Todo.year).all()
+    return render_template('index.html', books=books)
+
+@app.route('/admin/books/', methods=['POST', 'GET'])
 def admin():
     if request.method == 'POST':
         content_name = request.form['name']
@@ -30,9 +34,20 @@ def admin():
         except:
             return 'There was an issue adding your task'
 
-    else:
+    else:    
         books = Todo.query.order_by(Todo.year).all()
-        return render_template('index.html', books=books)
+        return render_template('admin.html', books=books)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    book_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(book_to_delete)
+        db.session.commit()
+        return redirect('/admin/books')
+    except:
+        return 'There was a problem deleting that task'
 
 if __name__ == '__main__':
     app.run(debug=True)
